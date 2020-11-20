047ec656ae51e814e5c55471d9933944b2e21584
Gregory Boyd <gboyd69@yahoo.com>
2020-11-19 20:04:38 -0500

    completed user default and saved data view

---
+++ W&BUserDefaults/SavedWeights.swift
@@ -0,0 +1,16 @@
+//
+//  SavedWeights.swift
+//  W&BUserDefaults
+//
+//  Created by Gregory Boyd on 11/12/20.
+//
+
+import Foundation
+
+class SavedWeights: ObservableObject {
+    var aircraft: String = ""
+    
+    var grossWeight: String = ""
+    
+    var cg: String = ""
+}
